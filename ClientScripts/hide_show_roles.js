f18cc0e4e61e6c034ac894b69e80537fa82924cb
vld62 <iliev62@gmail.com>
2019-01-14 10:37:22 +0200

    Adding changes to scripts

--- ClientScripts/hide_show_roles.js
+++ ClientScripts/hide_show_roles.js
@@ -1,10 +1,17 @@
 function onLoad() {   
     if (g_user.hasRole('admin')) {   
-            g_form.setDisplay('u_short_description_decrypt',true);   
-            g_form.setDisplay('u_description_decrypt',true);   
+		if (g_form.getValue('short_description') != '') {
+			g_form.setDisplay('u_short_description_decrypt',true);   
+            g_form.setDisplay('u_description_decrypt',true);  
+		    g_form.setDisplay('description',false);   
+            g_form.setDisplay('short_description',false);   
+		}
+
  
     }   else {
-		 g_form.setDisplay('u_short_description_decrypt',false);   
-         g_form.setDisplay('u_description_decrypt',false);   
+			 g_form.setDisplay('u_short_description_decrypt',false);   
+			 g_form.setDisplay('u_description_decrypt',false); 
+			 g_form.setDisplay('description',true);   
+             g_form.setDisplay('short_description',true); 
 	}
 }
