c8864d2082563edf92816226e420c78f22d29772
Vladislav Iliev <vladislav.iliev@doitwise.com>
2019-01-16 17:45:04 +0200

    adding changes

--- ScriptIncludes/setAssignmentGroupBS.js
+++ ScriptIncludes/setAssignmentGroupBS.js
@@ -1,11 +1,11 @@
 var setAssignmentGroupBS = Class.create();
 setAssignmentGroupBS.prototype = Object.extendsObject(AbstractAjaxProcessor, {
 	populateAssignmentGroup : function (){
-		var gr = new GlideRecord('cmdb_ci');
-		gr.addQuery ('sys_id',this.getParameter('sysparm_cmdb_ci'));
-		gr.query();
-		if (gr.next()) {
-			return gr.support_group;
+		var gr3 = new GlideRecord('cmdb_ci_service');
+		gr3.addQuery ('sys_id',this.getParameter('sysparm_cmdb_ci_service'));
+		gr3.query();
+		if (gr3.next()) {
+			return gr3.support_group;
 		}
 	},
 	type: 'setAssignmentGroupBS'
